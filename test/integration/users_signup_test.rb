2937df665429189e65e362dd435414014dd92fd4
João Bagio <34991522+jbagio@users.noreply.github.com>
2019-06-06 15:59:08 +0100

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'should not save user if invalid signup' do
+    get signup_path # optional here, added for conceptual clarity
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'foo@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test 'should save user if valid signup' do
+    get signup_path # optional here, added for conceptual clarity
+
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'foo',
+                                         email: 'foo@bar.com',
+                                         password: 'foobar',
+                                         password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
